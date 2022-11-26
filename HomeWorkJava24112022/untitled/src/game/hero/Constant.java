cfaf8a34f908d9cb15d74ce8d38d510c9ac4383a
Mikalai Babushkin <crazytravelist@gmail.com>
2022-11-26 21:01:35 +0100

    homework 22.11, 24,11 completed

---
+++ HomeWorkJava24112022/untitled/src/game/hero/Constant.java
@@ -0,0 +1,8 @@
+package game.hero;
+
+public class Constant {
+
+    final static int GNOME_POWER = 100;
+    final static int GNOME_CHARISMA = 100;
+    final static int GNOME_MAGIC = 100;
+}
---
+++ HomeWorkJava24112022/untitled/src/game/hero/Gnome.java
@@ -0,0 +1,15 @@
+package game.hero;
+
+import java.math.BigDecimal;
+
+public class Gnome {
+
+    private long id;
+    private int hitPoints;
+    private int spellPoints;
+    private int experiencePoints;
+    private String name;
+    private String clan;
+    private BigDecimal money;
+
+}
